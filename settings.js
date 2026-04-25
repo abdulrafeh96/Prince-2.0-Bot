9ede23d88cf9d5e86b8f4cf5b3595006ed897062
Abdul Rafeh <bc230406853ara@vu.edu.pk>
2026-04-25 13:12:19 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,17 @@
+const settings = {
+  packname: 'Prince 2.0',
+  author: '‎',
+  botName: "Prince 2.0",
+  botOwner: 'Abdul', // Your name
+  ownerNumber: '923195845103', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
+
